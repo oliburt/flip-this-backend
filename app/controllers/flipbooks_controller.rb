1ab57fd03b1fa7714f414658edff0e90ad2647a4
oliburt <oliburt@gmail.com>
2019-10-03 22:10:37 +0100

    added patch and delete flipbooks

--- app/controllers/flipbooks_controller.rb
+++ app/controllers/flipbooks_controller.rb
@@ -7,4 +7,19 @@ class FlipbooksController < ApplicationController
         end
     end
 
+    def update
+        flipbook = Flipbook.find(params[:id])
+        if flipbook.update(flipbook_object: params[:flipbook_object])
+            render json: flipbook
+        end
+    end
+
+    def destroy
+        flipbook = Flipbook.find(params[:id])
+        if flipbook
+            flipbook.destroy
+            render json: flipbook
+        end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   get"/users/:username", to: "users#show"
   
-  resources :flipbooks
+  resources :flipbooks, only: [:create, :update, :destroy]
   resources :users, only: [:create]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
