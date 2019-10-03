fce2205c617e922aa9c841f611d274e85799c9f9
oliburt <oliburt@gmail.com>
2019-10-03 12:01:58 +0100

    merged styles

--- app/controllers/flipbooks_controller.rb
+++ app/controllers/flipbooks_controller.rb
@@ -1,10 +1,5 @@
 class FlipbooksController < ApplicationController
 
-
-    # def index
-    #     render json: 
-    # end
-
     def create
         flipbook = Flipbook.new(user_id: params[:user_id], flipbook_object: params[:flipbook_object])
         if flipbook.save
